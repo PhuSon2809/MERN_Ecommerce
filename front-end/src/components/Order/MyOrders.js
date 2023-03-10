0a5e245a94fe4f8ba7aea316b80917608f22109d
DangLeHoaiVu <vudlhse150296@fpt.edu.vn>
2023-03-11 04:22:01 +0700

    order

--- front-end/src/components/Order/MyOrders.js
+++ front-end/src/components/Order/MyOrders.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import { DataGrid } from "@material-ui/data-grid";
-// import "./MyOrders.css";
+import "./MyOrders.css";
 import { useSelector, useDispatch } from "react-redux";
 import { clearErrors, myOrders } from "../../actions/orderAction";
 import Loader from "../layout/Loader/Loader";
@@ -9,46 +9,44 @@ import { useAlert } from "react-alert";
 import { Typography } from "@material-ui/core";
 import MetaData from "../layout/MetaData";
 import LauchIcon from "@material-ui/icons/Launch";
+import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 
 const MyOrders = () => {
   const dispatch = useDispatch();
   const alert = useAlert();
 
   const { loading, error, orders } = useSelector((state) => state.myOrders);
-  const { user } = useSelector((state) => state.user);
+  // const { user } = useSelector((state) => state.user);
 
   const columns = [
-    { field: "id", headerName: "Order ID", minWidth: 300, flex: 1 },
+    { field: "id", headerName: "Order ID", minWidth: 250 },
+    // { field: "name", headerName: "Order Name", minWidth: 300 },
     {
       field: "status",
       headerName: "Status",
-      minWidth: 150,
-      flex: 0.5,
+      minWidth: 250,
       cellClassName: (params) => {
         return params.getValue(params.id, "status") === "Delivered"
-        ? "greenColor"
-        : "redColor"
+          ? "greenColor"
+          : "redColor";
       },
     },
     {
       field: "itemQty",
       headerName: "Item Qty",
       type: "number",
-      minWidth: 150,
-      flex: 0.3,
+      minWidth: 253 + 25,
     },
     {
       field: "amount",
       headerName: "Amount",
       type: "number",
-      minWidth: 270,
-      flex: 0.5,
+      minWidth: 253 + 25,
     },
     {
       field: "actions",
-      flex: 0.3,
       headerName: "Actions",
-      minWidth: 150,
+      minWidth: 250,
       type: "number",
       sortable: false,
       renderCell: (params) => {
@@ -60,7 +58,11 @@ const MyOrders = () => {
       },
     },
   ];
-  const rows = [];
+  const orderTest = [
+    { id: 1, status: "Delivered", itemQty: 10, amount: 5 },
+    { id: 2, status: "Delivered", itemQty: 10, amount: 2 },
+  ];
+  const rows = orderTest;
 
   orders &&
     orders.forEach((item, index) => {
@@ -82,22 +84,32 @@ const MyOrders = () => {
 
   return (
     <Fragment>
-      <MetaData title={`${user.name} - Orders`} />
+      {/* <MetaData title={`${user.name} - Orders`} /> */}
+      <MetaData title={`Orders`} />
       {loading ? (
         <Loader />
       ) : (
         <div className="myOrdersPage">
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={10}
-            rowsPerPageOptions={[10]}
-            disableSelectionOnClick
-            className="myOrdersTable"
-            autoHeight
-          />
+          {rows.length === 0 ? (
+            <div className="emptyCart">
+              <RemoveShoppingCartIcon />
+              <Typography>You Have No order</Typography>
+              <Link to="/products">View Products</Link>
+            </div>
+          ) : (
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={10}
+              rowsPerPageOptions={[10]}
+              disableSelectionOnClick
+              className="myOrdersTable"
+              autoHeight
+            />
+          )}
 
-          <Typography id="myOrdersHeading">{user.name}'s Orders</Typography>
+          {/* <Typography id="myOrdersHeading">{user.name}'s Orders</Typography> */}
+          <Typography id="myOrdersHeading">Your Orders</Typography>
         </div>
       )}
     </Fragment>
